ce253e1fefb64cf8ade1273b87aa8ae341f8f8e3
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-25 18:19:29 +1200

    added react icons, set up working links on icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+
+// Components
+import Profile from './Components/Home/Profile';
 
 function App() {
   return (
     <div className="App">
-      working
+      <Profile />
     </div>
   );
 }
--- src/Components/Home/Profile.js
+++ src/Components/Home/Profile.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Profile.css'
+import { AiOutlineInstagram, AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
+
 
 const Profile = () => {
-   return (
-      <div>
-
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href='https://www.instagram.com/tainerasmussen/' target='_blank'>
+              <i><AiOutlineInstagram /></i>
+            </a>
+            <a href='https://www.linkedin.com/in/taine-rasmussen-a66a86184/' target='_blank'>
+              <i><AiFillLinkedin /></i>
+            </a>
+            <a href='https://github.com/taine-rasmussen' target='_blank'>
+              <i><AiFillGithub /></i>
+            </a>
+          </div>
+        </div>
       </div>
-   )
+    </div>
+  )
 }
 
 export default Profile
